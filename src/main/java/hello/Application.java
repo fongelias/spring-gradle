c51e83413b8b8ffb417b0421f905eb03b01dbf88
fongelias <fongelias@gmail.com>
2017-08-15 09:44:31 +0200

    updated app to remove rabbit mq

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,15 +1,7 @@
 package hello;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 //Adds configuration + enable autoconfiguration + component scan annotations
 //configuration tags the class as a source of bean definitions for the application context
@@ -17,47 +9,8 @@ import org.springframework.context.annotation.Bean;
 //component scan tells spring to look for other components in the hello package
 @SpringBootApplication
 public class Application {
-	//To use RabbitMQ, you need to configure a message listener container, 
-	//queue, exchange, binding between them, and a component to send messages
 
-	//We will use RabbitTemplate to send messages and register a Receiver with the message listener
-	//Connection factory allows both to connect with the RabbitMQ server
-
-	final static String queueName = "spring-boot";
-
-	//Used to explicitly declare a single bean
-	//Beans are objects with private properties, a constructor with no arguments, and implements Serializable
-	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
-	}
-
-	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange("spring-boot-exchange");
-	}
-
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with(queueName);
-	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-			MessageListenerAdapter listenerAdapter) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
-	}
-
-	public static void main(String[] args) throws InterruptedException {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
